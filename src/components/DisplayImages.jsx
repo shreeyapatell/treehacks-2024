13d27f60c8759885e674128f0b13cf94c5bea278
Shreeya Patel <shreeyapaatel@gmail.com>
2024-02-18 03:28:16 -0800

    images

--- src/components/DisplayImages.jsx
+++ src/components/DisplayImages.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 
 const DisplayImages = () => {
   // Dynamically import all image filenames from the images folder
-  const imageContext = require.context('/Users/shreeyapatel/Projects/Hackathons/treehacks/memory-playground/images', false, /\.(png|jpe?g|svg)$/);
-  const imageNames = imageContext.keys();
+  const imageContext = require.context('/Users/shreeyapatel/Projects/Hackathons/treehacks/memory-playground/images/', false, /\.(png|jpe?g|svg)$/);
+  const images = imageContext.keys().map(imageContext);
+
+  console.log("../../images/" + images)
 
   return (
     <div>
       <h2>Images</h2>
       <div className="container">
-        {imageNames.map((imageName, index) => (
+        {images.map((images, index) => (
           <img
             key={index}
-            src={imageName}
-            alt={imageName}
-            className="image"
+            src={images}
+            alt={'image ${index}'}
+            className="generated-image"
           />
         ))}
       </div>
